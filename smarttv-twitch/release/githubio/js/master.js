8ddfcf64f7bf4e99b6d700ced65199a6f5732106
Felipe Leon <fglfgl27@gmail.com>
2018-09-10 23:11:01 -0300

    Update master.js
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- smarttv-twitch/release/githubio/js/master.js
+++ smarttv-twitch/release/githubio/js/master.js
@@ -1,6 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
+/* jshint undef: true, unused: true, node: true, browser: true */
+
+Start();
+
+function Start() {
+    if (document.readyState === "loading") document.addEventListener("DOMContentLoaded", AfterLoading);
+    else AfterLoading(); // `DOMContentLoaded` already fired
+}
+
+function AfterLoading() {
     var master = document.createElement('script');
     master.src = 'https://fgl27.github.io/smarttv-twitch/release/githubio/js/master.js';
     master.defer = true;
     document.getElementsByTagName("head")[0].appendChild(master);
-});
+}
